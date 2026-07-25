db4219d502a60cc8da2315ba97dbf98ba0a69ad6
OKZGN <30320448+okzgn@users.noreply.github.com>
2026-07-24 23:46:01 -0500

    Update.

--- main_commons.js
+++ main_commons.js
@@ -1,26 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Main commons ready.');
+  console.log('Main commons ready.');
+
+  var menuButton = document.getElementById('menu');
+  var scrollContainer = document.querySelector('main');
+  var navContainer = document.querySelector('nav');
+  var navTitle = document.querySelector('h2.nav-title');
+  var navMainTitle = document.querySelector('h1.main-title');
 
   function showOrHideMenu(target) {
-      return function () {
-        if(target[target === window ? 'scrollY' : 'scrollTop'] > 96){
-            menuButton.classList.add('show');
+    return function () {
+      clearTimeout(showOrHideMenu.timeout);
+      showOrHideMenu.timeout = setTimeout(function () {
+        if (target[target === window ? 'scrollY' : 'scrollTop'] > 96) {
+          menuButton.classList.add('show');
+          navContainer.style.display = 'flex';
+          navTitle.textContent = navMainTitle.textContent;
         }
         else {
-            menuButton.classList.remove('show');
+          menuButton.classList.remove('show');
+          navContainer.style.display = 'none';
+          navTitle.textContent = '';
         }
-      }
+      }, 500);
     }
+  }
 
-    var menuButton = document.getElementById('menu');
-    var scrollContainer = document.querySelector('main');
-
-    window.addEventListener('scroll', showOrHideMenu(window));
-    scrollContainer.addEventListener('scroll', showOrHideMenu(scrollContainer));
-
-  menuButton.addEventListener('click', function (event) {
-    event.preventDefault();
+  function returnToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     scrollContainer.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  window.addEventListener('scroll', showOrHideMenu(window));
+  scrollContainer.addEventListener('scroll', showOrHideMenu(scrollContainer));
+
+  menuButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    returnToTop();
   });
 });
