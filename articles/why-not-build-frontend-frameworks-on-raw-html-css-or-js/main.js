0b651bd89957e2d6a2084a3f9fae9defc54e06f3
OKZGN <30320448+okzgn@users.noreply.github.com>
2026-08-18 09:47:24 -0500

    Article update.

--- articles/why-not-build-frontend-frameworks-on-raw-html-css-or-js/main.js
+++ articles/why-not-build-frontend-frameworks-on-raw-html-css-or-js/main.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Article Ready.');
+  hljs.configure({ cssSelector: 'pre code, .inline-code' });
+  hljs.highlightAll();
 });
